7e15aa90985179c46cac59ded2c4cdd68e7c709e
Kimberly Anderson <kimanderson05@fullsail.edu>
2014-04-19 18:49:41 -0400

    Copied Login to CRUD

---
+++ CRUD/views/details.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kim
+ * Date: 4/10/14
+ * Time: 9:03 PM
+ */
+
+echo <<< __END
+
+<h2>${row['firstName']} ${row['lastName']}</h2>
+<ul class="ul_info">
+    <li class="li_info">Username: ${row['userName']}</li>
+    <li class="li_info">Password: ${row['userPassword']}</li>
+</ul>
+
+__END;
